359425bbde9eef45667847b82e239dee96ca175d
Vladimir Olennikov <ath31st@yandex.ru>
2022-11-01 13:02:51 +0300

    add shop1 module with controller, service, dto inside

---
+++ shop1_service/src/main/java/top/shop/shop1_service/Shop1ServiceApplication.java
@@ -0,0 +1,13 @@
+package top.shop.shop1_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shop1ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shop1ServiceApplication.class, args);
+	}
+
+}
---
+++ shop1_service/src/test/java/top/shop/shop1_service/Shop1ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package top.shop.shop1_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shop1ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
