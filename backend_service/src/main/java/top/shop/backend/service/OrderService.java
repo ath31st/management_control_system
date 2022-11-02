2e9edc8014861e5a06a868ad678bbafbf9906954
Vladimir Olennikov <ath31st@yandex.ru>
2022-11-02 17:31:35 +0300

    little fix yml shop1, test cycling messages in project

--- backend_service/src/main/java/top/shop/backend/service/OrderService.java
+++ backend_service/src/main/java/top/shop/backend/service/OrderService.java
@@ -1,11 +1,14 @@
 package top.shop.backend.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import top.shop.backend.dto.DeliveryOrderDto;
 import top.shop.backend.dto.OrderDto;
 import top.shop.backend.entity.Order;
 import top.shop.backend.repository.OrderRepository;
+import top.shop.backend.util.Producer;
 
 @Slf4j
 @Service
@@ -15,7 +18,9 @@ public class OrderService {
     private final ShopService shopService;
     private final ProductService productService;
 
-    public void persistOrder(OrderDto orderDto) {
+    private final Producer producer;
+
+    public void persistOrder(OrderDto orderDto) throws JsonProcessingException {
         Order order = Order.builder()
                 .totalPrice(productService.getTotalPrice(orderDto.getAmount(),orderDto.getProductName()))
                 .amount(orderDto.getAmount())
@@ -26,6 +31,16 @@ public class OrderService {
                 .shop(shopService.getShop(orderDto.getShopName()))
                 .build();
         Order persistedOrder = orderRepository.save(order);
+
+
+        //TODO remove this block to standalone service
+        DeliveryOrderDto deliveryOrderDto = new DeliveryOrderDto();
+        deliveryOrderDto.setAmount(persistedOrder.getAmount());
+        deliveryOrderDto.setCustomerName(persistedOrder.getCustomerName());
+        deliveryOrderDto.setProductName(persistedOrder.getProductName());
+        deliveryOrderDto.setTotalPrice(persistedOrder.getTotalPrice());
+        deliveryOrderDto.setShopName(persistedOrder.getShop().getName());
+        producer.sendMessage(deliveryOrderDto);
 
         log.info("order received and persisted {}", persistedOrder);
     }
