76e094a2bfaaac89f33db52d2df2f1a6e9a86989
Vladimir Olennikov <ath31st@yandex.ru>
2022-11-02 11:27:33 +0300

    add backend service and relocate consumer from gateway to backend

--- gateway_service/src/main/java/top/shop/gateway/service/OrderService.java
+++ gateway_service/src/main/java/top/shop/gateway/service/OrderService.java
@@ -4,20 +4,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-import top.shop.gateway.dto.OrderDto;
-import top.shop.gateway.entity.Order;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class OrderService {
     private final ModelMapper modelMapper;
-
-    public void persistOrder(OrderDto OrderDto) {
-        Order order = modelMapper.map(OrderDto, Order.class);
-        Order persistedOrder = order;
-
-        log.info("order persisted {}", persistedOrder);
-    }
 
 }
--- gateway_service/src/main/java/top/shop/gateway/util/Consumer.java
+++ gateway_service/src/main/java/top/shop/gateway/util/Consumer.java
@@ -7,26 +7,22 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-import top.shop.gateway.dto.OrderDto;
 import top.shop.gateway.service.OrderService;
 
-@Slf4j
-@Component
-@RequiredArgsConstructor
-public class Consumer {
-   // private static final String orderTopic = "${topic.name}";
-    private static final String orderTopic = "t.order";
-
-    private final ObjectMapper objectMapper;
-    private final OrderService orderService;
-
-
-    @KafkaListener(topics = orderTopic)
-    public void consumeMessage(String message) throws JsonProcessingException {
-        log.info("message consumed {}", message);
-
-        OrderDto orderDto = objectMapper.readValue(message, OrderDto.class);
-        orderService.persistOrder(orderDto);
-    }
-
-}
+//@Slf4j
+//@Component
+//@RequiredArgsConstructor
+//public class Consumer {
+//    private static final String orderTopic = "${topic.name}";
+//
+//    private final ObjectMapper objectMapper;
+//    private final OrderService orderService;
+//
+//
+//    @KafkaListener(topics = orderTopic)
+//    public void consumeMessage(String message) throws JsonProcessingException {
+//        log.info("message consumed {}", message);
+//
+//    }
+//
+//}
