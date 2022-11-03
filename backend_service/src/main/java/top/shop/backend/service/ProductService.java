c18d4bced5aeba3c3fd0bd891cf82bde7a4469f9
Vladimir Olennikov <ath31st@yandex.ru>
2022-11-03 16:09:54 +0300

    add catalogue for customers

--- backend_service/src/main/java/top/shop/backend/service/ProductService.java
+++ backend_service/src/main/java/top/shop/backend/service/ProductService.java
@@ -2,8 +2,10 @@ package top.shop.backend.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import top.shop.backend.dto.ProductDto;
 import top.shop.backend.entity.Product;
 import top.shop.backend.exceptionhandler.exception.ProductException;
 import top.shop.backend.repository.ProductRepository;
@@ -16,6 +18,7 @@ import java.util.List;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final ModelMapper modelMapper;
 
     public String receiveProducts(List<Product> products) {
         productRepository.saveAll(products);
@@ -29,6 +32,13 @@ public class ProductService {
 
     public double getTotalPrice(int amount, String productName) {
         return getProduct(productName).getPrice() * (double) amount;
+    }
+
+    public List<ProductDto> getListProductDto() {
+        List<Product> products = productRepository.findAll();
+        return products.stream()
+                .map(p -> modelMapper.map(p, ProductDto.class))
+                .toList();
     }
 
 }
