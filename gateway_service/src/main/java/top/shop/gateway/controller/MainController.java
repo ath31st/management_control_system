769714fa2393d697318a22922e62e3883618c11c
Vladimir Olennikov <ath31st@yandex.ru>
2022-11-04 17:42:08 +0300

    fix register.html and MainController

--- gateway_service/src/main/java/top/shop/gateway/controller/MainController.java
+++ gateway_service/src/main/java/top/shop/gateway/controller/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.server.ResponseStatusException;
 import top.shop.gateway.dto.UserDto;
@@ -32,7 +33,7 @@ public class MainController {
     }
 
     @PostMapping("/register")
-    public String userRegistration(@Valid UserDto userData, BindingResult bindingResult, Model model){
+    public String userRegistration(@Valid @ModelAttribute("userData") UserDto userData, BindingResult bindingResult, Model model){
         if(bindingResult.hasErrors()){
             model.addAttribute("userData", userData);
             return "register";
