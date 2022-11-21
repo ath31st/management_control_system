0f8d65c08d0da331bec05dcce584d994ba33db95
Vladimir Olennikov <ath31st@yandex.ru>
2022-11-21 17:33:46 +0300

    trying integrated keycloak auth in project. step 2.1

--- gateway_service/src/main/java/top/shop/gateway/controller/MainController.java
+++ gateway_service/src/main/java/top/shop/gateway/controller/MainController.java
@@ -1,27 +1,17 @@
 package top.shop.gateway.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.server.ResponseStatusException;
-import top.shop.gateway.dto.UserDto;
-import top.shop.gateway.service.UserService;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-import java.security.Principal;
 
 
 @Controller
 @RequiredArgsConstructor
 public class MainController {
-
-    private final UserService userService;
 
     @GetMapping("/index")
     public String index(Model model) {
@@ -29,37 +19,10 @@ public class MainController {
         return "index";
     }
 
-//    @GetMapping("/login")
-//    public String login() {
-//        return "login";
-//    }
-//
-//    @GetMapping("/login-error")
-//    public String loginError(Model model) {
-//        model.addAttribute("loginError", true);
-//        return "login";
-//    }
-
-//    @GetMapping("/register")
-//    public String register(Model model) {
-//        model.addAttribute("userData", new UserDto());
-//        return "register";
-//    }
-//
-//    @PostMapping("/register")
-//    public String userRegistration(@Valid @ModelAttribute("userData") UserDto userData, BindingResult bindingResult, Model model) {
-//        if (bindingResult.hasErrors()) {
-//            model.addAttribute("userData", userData);
-//            return "register";
-//        }
-//        try {
-//            userService.registerUser(userData);
-//        } catch (ResponseStatusException e) {
-//            bindingResult.rejectValue("username", "userData.username", "An account already exists for this username.");
-//            model.addAttribute("userData", userData);
-//            return "register";
-//        }
-//        return "redirect:/index";
-//    }
+    @GetMapping(path = "/logout")
+    public String logout(HttpServletRequest request) throws ServletException {
+        request.logout();
+        return "/index";
+    }
 
 }
