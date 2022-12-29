fb3fb27b61e421f62843aad6fe0cc7d234c05f72
Vladimir Olennikov <ath31st@yandex.ru>
2022-12-29 16:14:28 +0300

    rework shop service from mongodb to postgres step 6 ProductPricingService complete

--- shop1_service/src/main/java/top/shop/shop1_service/repository/ProductPricingRepository.java
+++ shop1_service/src/main/java/top/shop/shop1_service/repository/ProductPricingRepository.java
@@ -4,9 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import top.shop.shop1_service.entity.ProductPricing;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductPricingRepository extends JpaRepository<ProductPricing, Long> {
     boolean existsByProduct_ServiceName(String serviceName);
-    ProductPricing findByProduct_ServiceName(String serviceName);
+
+    Optional<ProductPricing> findByProduct_ServiceName(String serviceName);
 
 }
--- shop1_service/src/main/java/top/shop/shop1_service/service/ProductPricingService.java
+++ shop1_service/src/main/java/top/shop/shop1_service/service/ProductPricingService.java
@@ -1,15 +1,18 @@
 package top.shop.shop1_service.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import top.shop.shop1_service.dto.product.ProductDto;
 import top.shop.shop1_service.dto.product.ProductPricingDto;
 import top.shop.shop1_service.entity.ProductPricing;
+import top.shop.shop1_service.exceptionhandler.exception.ProductPricingException;
 import top.shop.shop1_service.repository.ProductPricingRepository;
 import top.shop.shop1_service.util.wrapper.ProductPricingWrapper;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,40 +21,38 @@ public class ProductPricingService {
     private final ProductService productService;
 
     public void receiveProductPricingWrapperFromGateway(ProductPricingWrapper wrapper) {
-//        wrapper.getPricingDtoList().forEach(p -> {
-//                    if (productPricingExists(p.getProductServiceName())) {
-//                        updateProductPricing(p);
-//                    } else {
-//                        mongoTemplate.save(ProductPricing.builder()
-//                                .productServiceName(p.getProductServiceName())
-//                                .price(p.getPrice())
-//                                .build());
-//                    }
-//                }
-//        );
+        wrapper.getPricingDtoList().forEach(p -> {
+                    if (productPricingExists(p.getProductServiceName())) {
+                        updateProductPricing(p);
+                    } else {
+                        ProductPricing pp = new ProductPricing();
+                        pp.setPrice(p.getPrice());
+                        pp.setProduct(productService.getProduct(p.getProductServiceName()));
+                        productPricingRepository.save(pp);
+                    }
+                }
+        );
     }
 
-//    public ProductPricing getProductPricing(String productServiceName) {
-//        Optional<ProductPricing> pp = Optional.ofNullable(mongoTemplate.findById(productServiceName, ProductPricing.class));
-//        return pp.orElseThrow(() -> new ProductPricingException(HttpStatus.NOT_FOUND, "Product price with " + productServiceName + " not found!"));
-//    }
+    public ProductPricing getProductPricing(String productServiceName) {
+        Optional<ProductPricing> pp = productPricingRepository.findByProduct_ServiceName(productServiceName);
+        return pp.orElseThrow(() -> new ProductPricingException(HttpStatus.NOT_FOUND, "Product price with " + productServiceName + " not found!"));
+    }
 
-//    public ProductPricingDto getProductPricingDto(String productServiceName) {
-//        return modelMapper.map(getProductPricing(productServiceName), ProductPricingDto.class);
-//    }
+    public ProductPricingDto getProductPricingDto(String productServiceName) {
+        return productPricingMapper(getProductPricing(productServiceName));
+    }
 
-//    public void updateProductPricing(ProductPricingDto ppDto) {
-//        Query query = new Query();
-//        query.addCriteria(Criteria.where("productServiceName").is(ppDto.getProductServiceName()));
-//        Update update = new Update();
-//        update.set("price", ppDto.getPrice());
-//        mongoTemplate.findAndModify(query, update, ProductPricing.class);
-//    }
+    public void updateProductPricing(ProductPricingDto ppDto) {
+        ProductPricing pp = getProductPricing(ppDto.getProductServiceName());
+        pp.setPrice(pp.getPrice());
+        productPricingRepository.save(pp);
+    }
 
-//    public ProductDto updatePriceOfProductDto(ProductDto productDto) {
-//        productDto.setPrice(getProductPricingDto(productDto.getServiceName()).getPrice());
-//        return productDto;
-//    }
+    public ProductDto updatePriceOfProductDto(ProductDto productDto) {
+        productDto.setPrice(getProductPricingDto(productDto.getServiceName()).getPrice());
+        return productDto;
+    }
 
     public List<ProductPricingDto> getProductPricingDtoList(List<String> productServiceName) {
         return productPricingRepository.findAll().stream()
@@ -63,7 +64,7 @@ public class ProductPricingService {
 
     public ProductPricing addMockProductPricing(ProductDto productDto) {
         if (productPricingRepository.existsByProduct_ServiceName(productDto.getServiceName())) {
-            return productPricingRepository.findByProduct_ServiceName(productDto.getServiceName());
+            return getProductPricing(productDto.getServiceName());
         } else {
             ProductPricing pp = new ProductPricing();
             pp.setPrice(0);
@@ -71,10 +72,9 @@ public class ProductPricingService {
         }
     }
 
-//    public boolean productPricingExists(String productServiceName) {
-//        return mongoTemplate.exists(Query.query(Criteria.where("productServiceName")
-//                .is(productServiceName)), ProductPricing.class);
-//    }
+    public boolean productPricingExists(String productServiceName) {
+        return productPricingRepository.existsByProduct_ServiceName(productServiceName);
+    }
 
     private ProductPricingDto productPricingMapper(ProductPricing pp) {
         ProductPricingDto dto = new ProductPricingDto();
